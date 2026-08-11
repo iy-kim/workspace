08a4374a955d7e10e02b11522126baed36587ec3
iy-kim <eippoeo@gmail.com>
2026-08-11 23:57:35 +0900

    Start to study cpp

---
+++ cpp/test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <complex>
+
+void some_function(){
+	
+	//double  d = 2.2;
+	//int	i = 7;
+	
+	//d = d+i;
+	//i = d*i;
+	
+//41p
+	//double d1 = 2.3;
+	//double d2 {2.3};
+
+	//std::complex<double> z = 1;
+	//std::complex<double> z2{d1, d2};
+	//std::complex<double> z3 = {1,2};
+
+	//std::vector<int> v {1, 2, 3, 4, 5, 6};
+	//std::cout << d1 << " " << z2 <<"\n" << v[1] <<"\n";
+//42p
+
+	auto b =true;
+	std::cout<< b << "\n";
+}
+
+int main(){
+	some_function();
+}
